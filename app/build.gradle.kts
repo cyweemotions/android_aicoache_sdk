4e62da0cd42f08dccf636cb7036b79b9cbde653e
ligin <960157994@qq.com>
2024-11-26 15:07:44 +0800

    feat:添加pickView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.gsonlib)
+    implementation(libs.pickview)
 }
